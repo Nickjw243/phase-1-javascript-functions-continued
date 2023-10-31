953065d339bdf4c00c1d617ca62926b54e3c8964
Nick Weiland <njweiland23@gmail.com>
2023-10-31 16:07:27 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(result = '*') {
+    const part1 = "You are"
+    return function (emphatic = "a hard worker") {
+        return `${part1} ${result}${emphatic}${result}!`
+    }
+}
+
+wrapAdjective("||")( "a dedicated programmer")
